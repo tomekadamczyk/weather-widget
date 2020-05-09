810b59aed605ed68e11305419b64a7c7cc6ee068
Tomek A <tomasz_adamczyk@hotmail.com>
2020-05-09 12:39:36 +0200

    Sky Parameter component

---
+++ src/components/Parameters/SingleParameter/Sky/Sky.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import cloud from '../../../../assets/images/cloud.png';
+
+const SkyBlock = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    border-right: 2px solid ${({theme}) => theme.colors.gray3};
+`;
+
+const Icon = styled.img`
+    width: 51px;
+`;
+
+const Sky = ({value}) => {
+
+    return (
+        <SkyBlock>
+            <Icon src={cloud} alt={value} />
+        </SkyBlock>
+    )
+}
+
+export default Sky;
