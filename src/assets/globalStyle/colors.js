3a1499a4e4523d7f0e8a02a3be6a35000922a8bd
Tomek A <tomasz_adamczyk@hotmail.com>
2020-05-09 12:36:21 +0200

    new colors

--- src/assets/globalStyle/colors.js
+++ src/assets/globalStyle/colors.js
@@ -1,5 +1,8 @@
 export const colors = {
     gray: '#888888',
+    gray2: '#ddd',
+    gray3: '#eeeeee',
+    gray4: '#d7d7d7',
     blue: '#65cafa',
     yellow: '#f9d44e',
     gray_light: '#f0f0f0',
